56dfee6901e09df390c5add7aa5d3b02cec1b455
dilloncoffman <dillonscoffman@gmail.com>
2017-07-22 18:13:06 +0000

    Basic search functionality added

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var express = require('express');
 var app = express();
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+    res.render("search");
+});
+
 app.get("/results", function(req, res){
-    request("http://www.omdbapi.com/?s=guardians&apikey=thewdb", function(error, response, body){
+    var query = req.query.search; //save value obtained from key "search" in the search template form
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb"; //make url var and concatenate query so we have shorter request to make below
+    request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
             var data = JSON.parse(body);
             res.render("results", {data: data} );
