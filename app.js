8aa3f207d75882ed5e3e28f168a8fe055ede3df4
dilloncoffman <dillonscoffman@gmail.com>
2017-07-22 18:21:23 +0000

    Bolded title of movie, added year movie was released, and added a simple link to search again

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get("/", function(req, res) {
 
 app.get("/results", function(req, res){
     var query = req.query.search; //save value obtained from key "search" in the search template form
-    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb"; //make url var and concatenate query so we have shorter request to make below
+    var url = "http://www.omdbapi.com/?s=" + query + "&apikey=thewdb"; //make url var and concatenate query so we have shorter request to make below, makes it dynamic
     request(url, function(error, response, body){
         if(!error && response.statusCode == 200){
             var data = JSON.parse(body);
